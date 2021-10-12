6f90703b5129be89a9cdc766bd028403dde9601c
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2021-10-12 21:49:32 +0100

    chore: clean up urls

--- components/ImageList/ImageList.js
+++ components/ImageList/ImageList.js
@@ -3,28 +3,28 @@
 const host = 'https://images.unsplash.com'
 const getFiles = ({ quality, format }) => [
   {
-    source: `${host}/photo-1616927366799-d4cab1b7e545?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYzMzczNDAxMg&ixlib=rb-1.2.1&q=${quality}&format=${format}&w=1080`
+    source: `${host}/photo-1616927366799-d4cab1b7e545?crop=entropy&cs=tinysrgb&fit=max&q=${quality}&fm=${format}&w=1080`
   },
   {
-    source: `${host}/photo-1632222139524-46f1e74c7a00?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYzMzczNDAzOA&ixlib=rb-1.2.1&q=${quality}&format=${format}&w=1080`
+    source: `${host}/photo-1632222139524-46f1e74c7a00?crop=entropy&cs=tinysrgb&fit=max&q=${quality}&fm=${format}&w=1080`
   },
   {
-    source: `${host}/photo-1633110385650-f7a43ac6fce9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYzMzczNDA0Ng&ixlib=rb-1.2.1&q=${quality}&format=${format}&w=1080`
+    source: `${host}/photo-1633110385650-f7a43ac6fce9?crop=entropy&cs=tinysrgb&fit=max&q=${quality}&fm=${format}&w=1080`
   },
   {
-    source: `${host}/photo-1632071929769-2c42d1c29c6a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYzMzczNDA1MQ&ixlib=rb-1.2.1&q=${quality}&format=${format}&w=1080`
+    source: `${host}/photo-1632071929769-2c42d1c29c6a?crop=entropy&cs=tinysrgb&fit=max&q=${quality}&fm=${format}&w=1080`
   },
   {
-    source: `${host}/photo-1632259699908-793677e7ff13?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYzMzczNDA1Ng&ixlib=rb-1.2.1&q=${quality}&format=${format}&w=1080`
+    source: `${host}/photo-1632259699908-793677e7ff13?crop=entropy&cs=tinysrgb&fit=max&q=${quality}&fm=${format}&w=1080`
   },
   {
-    source: `${host}/photo-1633463960820-5be13de85a74?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYzMzczNDA1OQ&ixlib=rb-1.2.1&q=${quality}&format=${format}&w=1080`
+    source: `${host}/photo-1633463960820-5be13de85a74?crop=entropy&cs=tinysrgb&fit=max&q=${quality}&fm=${format}&w=1080`
   },
   {
-    source: `${host}/photo-1631649171966-60487005da38?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYzMzczNDM1MQ&ixlib=rb-1.2.1&q=${quality}&format=${format}&w=1080`
+    source: `${host}/photo-1631649171966-60487005da38?crop=entropy&cs=tinysrgb&fit=max&q=${quality}&fm=${format}&w=1080`
   },
   {
-    source: `${host}/photo-1631490000790-f42d842e7c7a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYzMzczNDM2Ng&ixlib=rb-1.2.1&q=${quality}&format=${format}&w=1080`
+    source: `${host}/photo-1631490000790-f42d842e7c7a?crop=entropy&cs=tinysrgb&fit=max&q=${quality}&fm=${format}&w=1080`
   }
 ]
 
