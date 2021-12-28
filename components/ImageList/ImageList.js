fc43a84bcbde0d3e7ae1c1bb81e7de91cd172f02
Vitalii <vvv.melnychuk@gmail.com>
2021-12-28 12:00:58 +0000

    feat: calculate images size

--- components/ImageList/ImageList.js
+++ components/ImageList/ImageList.js
@@ -1,30 +1,79 @@
 /* eslint-disable @next/next/no-img-element */
+import { useEffect, useState } from 'react'
+
 import { imagesUrls } from './data'
 
-const getFiles = ({ quality, format, fit, width, height }) =>
-  imagesUrls.map((imageUrl) => ({
-    source: `${imageUrl}?q=${quality}&fm=${format}&fit=${fit}&w=${width}&h=${height}`
-  }))
+const getImageSize = async (imgURL) => {
+  const filesize = await fetch(imgURL, { mode: 'cors' })
+    .then((response) => response.blob())
+    .then((blob) => Math.round(blob.size / 10) / 100)
+
+  return filesize
+}
+
+const getImageData = async (
+  imageUrl,
+  { quality, format, fit, width, height }
+) => {
+  const source = `${imageUrl}?q=${quality}&fm=${format}&fit=${fit}&w=${width}&h=${height}`
+  const size = await getImageSize(source)
+  console.log('req')
+
+  return {
+    source,
+    size
+  }
+}
+
+const getImagesData = async (params) => {
+  const images = await Promise.all(
+    imagesUrls.map(async (imageUrl) => getImageData(imageUrl, params))
+  )
+
+  const totalSize = images
+    .reduce((previousValue, { size }) => previousValue + size, 0)
+    .toFixed(2)
+
+  return { images, totalSize }
+}
 
 export default function Example({ quality, format, fit, width, height }) {
-  const files = getFiles({ quality, format, fit, width, height })
+  const [images, setImages] = useState({ images: [], totalSize: 0 })
+
+  const fetchImages = async (params) => {
+    const imagesData = await getImagesData(params)
+    setImages(imagesData)
+  }
+
+  useEffect(() => {
+    fetchImages({ quality, format, fit, width, height })
+  }, [quality, format, fit, width, height])
 
   return (
-    <ul
-      role="list"
-      className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
-    >
-      {files.map((file) => (
-        <li key={file.source} className="relative">
-          <div className="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
-            <img
-              src={file.source}
-              alt=""
-              className="object-cover pointer-events-none group-hover:opacity-75"
-            />
-          </div>
-        </li>
-      ))}
-    </ul>
+    <>
+      Total: {images.totalSize} Kb
+      <ul
+        role="list"
+        className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
+      >
+        {images.images.map((image) => (
+          <li key={image.source} className="relative">
+            <div className="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
+              <img
+                src={image.source}
+                alt={image.source}
+                className="object-cover pointer-events-none group-hover:opacity-75"
+              />
+            </div>
+            <div className="mt-4 flex justify-between">
+              <h3 className="text-sm text-gray-700">
+                <span aria-hidden="true" className="absolute inset-0" />
+                {image.size} Kb
+              </h3>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
